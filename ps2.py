4a1b5b8b01cec283114e278368d4fd923f955591
0011pawan <pawanyadav16998@gmail.com>
2023-06-09 23:31:18 -0700

    yes

--- ps2.py
+++ ps2.py
@@ -11,10 +11,12 @@ def convert_to_pdf(input_file, output_folder_path):
     val = file_extension.lower()
 
     if val in ['.mhtml', '.html', '.htm', '.igs']:
-        soup = BeautifulSoup(input_file, 'html.parser')
+        with open(input_file.name, 'rb') as file:
+            soup = BeautifulSoup(file, 'html.parser')
+
         data = soup.find_all()
 
-        output_file_name = os.path.splitext(input_file.name)[0] + ".pdf"
+        output_file_name = os.path.splitext(os.path.basename(input_file.name))[0] + ".pdf"
         output_file_path = os.path.join(output_folder_path, output_file_name)
 
         with open(output_file_path, 'wb') as output_file:
@@ -25,12 +27,13 @@ def convert_to_pdf(input_file, output_folder_path):
             pisa.CreatePDF(html, dest=output_file)
 
         st.write(f"File converted: {output_file_path}")
+        os.remove(input_file.name)
 
     elif val == '.pdf':
-        output_file_path = os.path.join(output_folder_path, input_file.name)
+        output_file_name = os.path.basename(input_file.name)
+        output_file_path = os.path.join(output_folder_path, output_file_name)
         with open(output_file_path, 'wb') as output_file:
             shutil.copyfileobj(input_file, output_file)
-
         st.write(f"File copied: {output_file_path}")
 
 
@@ -42,12 +45,13 @@ def scrape_files(input_files, output_folder_path):
 def main():
     st.title("File Conversion App")
     
-    input_files = st.file_uploader("Upload files", accept_multiple_files=True)
-    output_folder_path = st.text_input("Output Folder Path", value="/app/data/output")
-
-    if st.button("Scrape Files") and input_files:
+    input_folder_path = st.text_input('Input Folder Path')
+    output_folder_path = st.text_input('Output Folder Path')
+    
+    if st.button('Scrape Files') and input_folder_path and output_folder_path:
+        input_files = st.file_uploader("Upload files", type=["mhtml", "html", "htm", "igs"], accept_multiple_files=True)
         scrape_files(input_files, output_folder_path)
-        st.success("Files scraped successfully!")
+        st.success('Files scraped successfully!')
 
 
 if __name__ == '__main__':
