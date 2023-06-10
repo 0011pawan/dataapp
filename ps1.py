7e55abfa652328e36ae5da3ed0e9986ee163b963
0011pawan <pawanyadav16998@gmail.com>
2023-06-09 22:38:14 -0700

    as

---
+++ ps1.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+import shutil
+import tempfile
+from xhtml2pdf import pisa
+
+def convert_to_pdf(input_file, output_folder_path):
+    _, file_extension = os.path.splitext(input_file)
+    val = file_extension.lower()
+
+    if val in ['.mhtml', '.html', '.htm', '.igs']:
+        with open(input_file, 'rb') as file:
+            soup = BeautifulSoup(file, 'html.parser')
+        
+        data = soup.find_all()
+        
+        output_file_name = os.path.splitext(os.path.basename(input_file))[0] + ".pdf"
+        output_file_path = os.path.join(output_folder_path, output_file_name)
+        
+        with open(output_file_path, 'wb') as output_file:
+            html = "<html><head><meta charset='UTF-8'></head><body>"
+            for d in data:
+                html += str(d)
+            html += "</body></html>"
+            pisa.CreatePDF(html, dest=output_file)
+        
+        print(f"File converted: {output_file_path}")
+        os.remove(input_file)
+        
+    elif val == '.pdf':
+        output_file_name = os.path.basename(input_file)
+        output_file_path = os.path.join(output_folder_path, output_file_name)
+        shutil.copyfile(input_file, output_file_path)
+        os.remove(input_file)
+
+def scrape_files(input_folder_path, output_folder_path):
+    input_files = [os.path.join(input_folder_path, f) for f in os.listdir(input_folder_path) if os.path.isfile(os.path.join(input_folder_path, f))]
+
+    for input_file in input_files:
+        convert_to_pdf(input_file, output_folder_path)
+
+def main():
+    input_folder_path = st.text_input('Input Folder Path')
+    output_folder_path = st.text_input('Output Folder Path')
+    
+    if st.button('Scrape Files'):
+        scrape_files(input_folder_path, output_folder_path)
+        st.success('Files scraped successfully!')
+
+if __name__ == '__main__':
+    main()
