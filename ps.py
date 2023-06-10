cbf3933331a2bb1dc78aba0eee9f4250fc70b779
0011pawan <pawanyadav16998@gmail.com>
2023-06-10 06:33:24 -0700

    sssss

--- ps.py
+++ ps.py
@@ -72,8 +72,8 @@ def main():
 
     # Convert files to PDF and display success message
     if st.button('Scrape Files') and input_folder_path and output_folder_path:
-        input_folder_path = input_folder_path.strip("'")  # Remove the single quotes from the input folder path
-        output_folder_path = output_folder_path.strip("'")
+        input_folder_path = input_folder_path.strip("'").replace('\\\\', '\\')  # Remove the single quotes from the input folder path
+        output_folder_path = output_folder_path.strip("'").replace('\\\\', '\\')
         scrape_files(input_folder_path, output_folder_path)
         st.success('Files scraped successfully!')
 if __name__ == '__main__':
